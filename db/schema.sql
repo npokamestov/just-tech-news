16332ba74e9af0c77897037edfa2a499903d9a3d
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2020-10-29 17:33:34 -0400

    added vote routes to add and display votes appropriately

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
+-- DROP TABLE IF EXISTS votes;
+
 DROP DATABASE IF EXISTS just_tech_news_db;
 
+-- DROP TABLE IF EXISTS vote
+
 CREATE DATABASE just_tech_news_db;
+
+-- USE DATABASE just_tech_news_db;
