893c39ec6fb8b8d1c4259b33e30f23e350ec2244
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2020-11-05 15:55:33 -0500

    implemented sessions and added front end JS

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3307;
@@ -11,6 +14,17 @@ const PORT = process.env.PORT || 3307;
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+const sess= {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
