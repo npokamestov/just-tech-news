61f8d0494c18bbd542441d2f073a65219aba3e28
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2020-11-05 19:00:39 -0500

    started dashboard template

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -15,7 +15,7 @@ async function loginFormHandler(event) {
         });
 
         if (response.ok) {
-            document.location.replace('/');
+            document.location.replace('/dashboard');
             // console.log('success');
         } else {
             alert(response.statusText);
