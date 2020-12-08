e2822478a8e54850874c11a741b6b59fc12b622e
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2020-12-08 17:50:04 -0500

    update repo

--- models/Post.js
+++ models/Post.js
@@ -9,29 +9,29 @@ class Post extends Model {
             post_id: body.post_id
         }).then(() => {
             return Post.findOne({
-            where: {
-                id: body.post_id
-            },
-            attributes: [
-                'id',
-                'post_url',
-                'title',
-                'created_at',
-                [
-                    sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-                    'vote_count'
-                ]
-            ],
-            include: [
-                {
-                    model: models.Comment,
-                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                    include: {
-                        model: models.User,
-                        attributes: ['username']
+                where: {
+                    id: body.post_id
+                },
+                attributes: [
+                    'id',
+                    'post_url',
+                    'title',
+                    'created_at',
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                        'vote_count'
+                    ]
+                ],
+                include: [
+                    {
+                        model: models.Comment,
+                        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                        include: {
+                            model: models.User,
+                            attributes: ['username']
+                        }
                     }
-                }
-            ]
+                ]
             });
         });
     }
